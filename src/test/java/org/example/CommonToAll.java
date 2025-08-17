b2c8a6298cb492874daf154b0198fe1dda96d1f4
Sayan3o3 <sayangiri7777@gmail.com>
2025-08-17 12:04:03 +0530

    Selenium Basic 4 and % is added

--- src/test/java/org/example/CommonToAll.java
+++ src/test/java/org/example/CommonToAll.java
@@ -1,6 +1,16 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
 
 public class CommonToAll {
     public WebDriver driver;
@@ -10,5 +20,24 @@ public class CommonToAll {
     }
     public static void closedBrowser(WebDriver driver){
         driver.quit();
+    }
+    public static  void waitForVisibility(WebDriver driver,int timeInSeconds,String xpath){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeInSeconds));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+
+    }
+    public static void waitFluentVisibility(WebDriver driver, int maxTime, int pollingTime, String xpath) {
+
+        FluentWait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(maxTime))
+                .pollingEvery(Duration.ofSeconds(pollingTime)).ignoring(NoSuchElementException.class);
+
+        WebElement error_message = wait.until(new Function<WebDriver, WebElement>() {
+            @Override
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement((By.xpath(xpath)));
+            }
+        });
     }
 }
